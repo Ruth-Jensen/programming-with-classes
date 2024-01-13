9d2422f78f31987b27a00399437a474500d5afcb
Ruth-Jensen <ruthjensen@byui.edu>
2024-01-13 14:14:33 -0700

    I wrote some code that gets a score from the user, then converts it from a string to an int

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //A >= 90 B >= 80 C >= 70 D >= 60 F < 60
+        Console.Write("What score did you get in your class?: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
     }
 }
