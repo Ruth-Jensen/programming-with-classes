7bf6ab4f819561e6d8cb68d83db81c1bedf91239
Ruth-Jensen <ruthjensen@byui.edu>
2024-01-16 13:26:44 -0700

    I added some notes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+
+        //this is a single line comment
+        
+        /*this is a
+        multiline comment */
+
+        //ctrl forward slash on a line to comment it out
     }
 }
