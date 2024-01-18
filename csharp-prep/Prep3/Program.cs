26c227f8a5dfc6258cea8b6f04431b9b3bf5a925
Ruth Jensen <ruthjensen@byui.edu>
2024-01-18 13:18:58 -0700

    stretch challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,26 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
-        int guess = 0;
-
-        while (guess != magicNumber)
+        bool keepPlaying = true;
+        while (keepPlaying)
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            string userInput = "";
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = 0;
+            int guessNum = 0;
 
-            if (guess > magicNumber)
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Lower");
+                guessNum += 1;
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessNum} trys.");
+                }
             }
-            else if (guess < magicNumber)
+
+            while (true)
             {
-                Console.WriteLine("Higher");
+                Console.Write("Would you like to play again? ");
+                userInput = Console.ReadLine().ToUpper();
+
+                if (userInput == "YES")
+                {
+                    break;
+                }
+                else if (userInput == "NO")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter YES or NO.");
+                }
             }
-            else
+            if (userInput == "NO")
             {
-                Console.WriteLine("You guessed it!");
+                keepPlaying = false;
             }
         }
     }
