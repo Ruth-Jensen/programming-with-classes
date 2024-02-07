4f310f94cb762fdc0a2703e5cb84b227a02f6138
Ruth Jensen <ruthjensen@byui.edu>
2024-02-07 13:45:38 -0700

    completed assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // 1
+        // 5
+        // 3/4
+        // 1/3
+
+        Fraction fraction = new Fraction();
+
+        string fractionString = fraction.GetFractionString();
+        double fractionDouble = fraction.GetDecimalValue();
+        Console.WriteLine(fractionString);
+        Console.WriteLine(fractionDouble);
+
+        fraction.SetTop(5);
+        fractionString = fraction.GetFractionString();
+        fractionDouble = fraction.GetDecimalValue();
+        Console.WriteLine(fractionString);
+        Console.WriteLine(fractionDouble);
+
+        fraction.SetTop(3);
+        fraction.SetBottom(4);
+        fractionString = fraction.GetFractionString();
+        fractionDouble = fraction.GetDecimalValue();
+        Console.WriteLine(fractionString);
+        Console.WriteLine(fractionDouble);
+
+        fraction.SetTop(1);
+        fraction.SetBottom(3);
+        fractionString = fraction.GetFractionString();
+        fractionDouble = fraction.GetDecimalValue();
+        Console.WriteLine(fractionString);
+        Console.WriteLine(fractionDouble);
     }
 }
